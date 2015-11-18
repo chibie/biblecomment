8d3642a46bab45eef3005321a52cdf5f7105e0f6
chibie <chibuotu@gmail.com>
2015-11-18 09:59:56 +0100

    modified models

--- mybiblecomment/models.py
+++ mybiblecomment/models.py
@@ -1,18 +1,17 @@
 from django.db import models
-from bible.models import Verse
 from django.conf import settings
 
 
 class Comment(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
-    verse = models.OneToOneField(Verse, related_name="comment")
+    verse = models.OneToOneField('bible.Verse', related_name='comment')
     text = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     was_blessed = models.PositiveIntegerField(default=0)
 
     def _get_reference(self):
-        """Returns bible verse complete reference"""
+        # Returns bible verse complete reference
         return "{0} {1}:{2}".format(self.verse.chapter.book.name, self.verse.chapter.number, self.verse.number)
 
     reference = property(_get_reference)
