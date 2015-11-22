8b26bc70fcfa15d172b95d85e510865a59f31c82
chibie <chibuotu@gmail.com>
2015-11-22 21:07:07 +0100

    added view for listing comments

--- mybiblecomment/views.py
+++ mybiblecomment/views.py
@@ -8,19 +8,19 @@ from django.views.generic.edit import (
     FormMixin, CreateView
 )
 from mybiblecomment.models import Comment
-from accounts.forms import LoginForm
+from mybiblecomment.forms import ScriptureLookupForm
 
 import datetime
 
 
 class IndexView(FormMixin, TemplateView):
-    form_class = LoginForm
+    form_class = ScriptureLookupForm
     template_name = 'biblecomment/index.html'
-    success_url = reverse_lazy('biblecomment:index')
+    success_url = reverse_lazy('biblecomment:comment-list')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['login_form'] = self.get_form()
+        context['scripture_lookup_form'] = self.get_form()
         context['latest_comments'] = Comment.objects.order_by('-pub_date')
         context['trending_comments'] = Comment.objects.filter(
             pub_date__gte=timezone.now()-datetime.timedelta(weeks=1)).order_by('-was_blessed', '-pub_date')
@@ -29,10 +29,40 @@ class IndexView(FormMixin, TemplateView):
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         if form.is_valid():
-            login(request, form.get_user())
+            request.session['book'] = form.fields['book'].queryset[int(request.POST['book'])-1].name
+            request.session['chapter'] = request.POST['chapter']
+            request.session['start_verse'] = request.POST['start_verse']
+            request.session['end_verse'] = request.POST['end_verse']
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
+
+
+class CommentListView(TemplateView):
+    template_name = 'biblecomment/comment_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CommentListView, self).get_context_data(**kwargs)
+        if int(self.request.session['start_verse']) == int(self.request.session['end_verse']):
+            context['reference'] = "{0} {1}:{2}".format(
+                self.request.session['book'],
+                self.request.session['chapter'],
+                self.request.session['start_verse']
+            )
+        elif int(self.request.session['start_verse']) < int(self.request.session['end_verse']):
+            context['reference'] = "{0} {1}:{2}-{3}".format(
+                self.request.session['book'],
+                self.request.session['chapter'],
+                self.request.session['start_verse'],
+                self.request.session['end_verse']
+            )
+        context['comments'] = Comment.objects.filter(
+            book__name=self.request.session['book'],
+            chapter=self.request.session['chapter'],
+            start_verse=self.request.session['start_verse'],
+            end_verse=self.request.session['end_verse']
+        )
+        return context
 
 
 class WriteCommentView(CreateView):
