9e35ec5a014c5a5bb0750e55058eb228eddc398d
chibie <chibuotu@gmail.com>
2015-11-22 21:08:51 +0100

    added urls

--- mybiblecomment/urls.py
+++ mybiblecomment/urls.py
@@ -4,5 +4,6 @@ from mybiblecomment.views import *
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index"),
     url(r'^index/', IndexView.as_view(), name="index"),
-    url(r'^write/', WriteCommentView.as_view(), name="write_comment")
+    url(r'^write/', WriteCommentView.as_view(), name="write_comment"),
+    url(r'^comments/', CommentListView.as_view(), name="comment-list")
 ]
