13af752be30bfc432e8139f03cd012e218c0e1b7
chibie <chibuotu@gmail.com>
2015-12-06 22:48:51 +0100

    updated urls

--- mybiblecomment/urls.py
+++ mybiblecomment/urls.py
@@ -4,6 +4,7 @@ from mybiblecomment.views import *
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index"),
     url(r'^index/', IndexView.as_view(), name="index"),
-    url(r'^write/', WriteCommentView.as_view(), name="write_comment"),
-    url(r'^comments/', CommentListView.as_view(), name="comment-list")
+    url(r'^write/', WriteCommentView.as_view(), name="write-comment"),
+    url(r'^comments/', CommentListView.as_view(), name="comment-list"),
+    url(r'^comment/(?P<pk>[0-9]+)/', CommentDetailView.as_view(), name="comment-detail"),
 ]
